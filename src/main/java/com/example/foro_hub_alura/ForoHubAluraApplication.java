44909d9c44b2bfdf15a215b6541ccd52f9b68da2
Merari May Sosa <merarimaysosa@gmail.com>
2024-06-12 13:12:38 -0600

    Creacion de proyecto inicial

---
+++ src/main/java/com/example/foro_hub_alura/ForoHubAluraApplication.java
@@ -0,0 +1,13 @@
+package com.example.foro_hub_alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foro_hub_alura/ForoHubAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foro_hub_alura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
